e891c2a63718840b65badf4d5ea299796170794c
NitishGhormade <118716634+NitishGhormade@users.noreply.github.com>
2023-02-19 22:21:58 +0530

    Update Form Validation.js

--- HTML/Form Validation/Form Validation.js
+++ HTML/Form Validation/Form Validation.js
@@ -1,53 +1,110 @@
 function valid() {
-    let idid = document.getElementById("fid").value;
+    let flag = true;
 
-    let fnfn = document.getElementById("fn").value;
-    let rfnfn = /[0-9]/;
+    let id = document.getElementById("fid").value;
 
-    let ppp = document.getElementByTd("fpp").value;
+    let name = document.getElementById("fn").value;
+    let rname = /[0-9]/;
 
-    let dd = document.getElementByTd("dd").value;
+    let pass = document.getElementById("fp").value;
 
-    let pin = document.getElementByTd("fpin").value;
-    let rpin = /[a-z A-Z]/;
+    let dd = document.getElementById("dd").value;
 
-    let fe = document.getElementByTd("fe").value;
-    let rfe = /([a-z A-Z 0-9 \.-_]+)@(^(gmail)$).(com)/;
+    let pin = document.getElementById("fpin").value;
+    let rpin = /[a-z A-Z .!@#$%^&*]/;
 
-    if (idid.length < 8 || idid.length > 12) {
-        document.getElementById("idlb").style.visibility = "visible";
-        document.getElementById("fid").style.border = "2px solid red";
-        return false;
-    }
+    let email = document.getElementById("fe").value;
+    let remail = /([a-z A-Z 0-9 -_]+)@([a-z A-Z]+).([a-z A-Z])/;
 
-    if (rfnfn.test(fnfn)) {
-        document.getElementById("nlb").style.visibility = "visible";
-        document.getElementById("fn").style.border = "2px solid red";
-        return false;
-    }
+    let  ckb1= document.getElementById("ckb1").checked;
+    let  ckb2= document.getElementById("ckb2").checked;
     
-    if(ppp.length < 7 || ppp.length > 12){
-        document.getElementById("pplb").style.visibility = "visible";
-        document.getElementById("fpp").style.border = "2px solid red";
-        return false;
+    // if (id == "") {
+    //     document.getElementById("idlb").innerHTML="*Please Fill";
+    //     document.getElementById("fid").style.border = "2px solid red";
+    //     flag = false;
+    //}
+    if (id.length < 5 || id.length > 12) {
+        document.getElementById("idlb").innerHTML="*5 < length < 12*";
+        document.getElementById("fid").style.border = "2px solid red";
+        flag = false;
+    }
+    else {
+        document.getElementById("idlb").innerHTML="";
+        document.getElementById("fid").style.border = "none";
     }
 
-    if(dd = select){
-        document.getElementById("ddlb").style.visibility = "visible";
+    if(rname.test(name)){
+        document.getElementById("nlb").innerHTML="*Digits Are NOT Allowed";
+        document.getElementById("fn").style.border="2px solid red";
+        flag = false;
+    }
+    else if(name.length <= 1){
+        document.getElementById("nlb").innerHTML="*Length Of NAME >= 2";
+        document.getElementById("fn").style.border="2px solid red";
+        flag = false;
+    }
+    else{
+        document.getElementById("nlb"),innerHTML="";
+        document.getElementById("fn").style.border="none";
+    }
+   
+    // if (name == "") {
+    //     document.getElementById("plb").innerHTML="*Please Fill";
+    //     document.getElementById("fp").style.border = "2px solid red";
+    //     flag = false;
+    // }
+    if (pass.length < 7 || pass.length > 12) {
+        document.getElementById("plb").innerHTML="*5 < length < 12*";
+        document.getElementById("fp").style.border = "2px solid red";
+        flag = false;
+    }
+    else {
+        document.getElementById("plb").innerHTML="";
+        document.getElementById("fp").style.border = "none";
+    }
+
+    if(dd == "select"){
+        document.getElementById("ddlb").innerHTML="*Must Select Country";
         document.getElementById("dd").style.border = "2px solid red";
-        return false;
+        flag = false;
+    }
+    else{
+        document.getElementById("ddlb").innerHTML="";
+        document.getElementById("dd").style.border = "none";
     }
 
-    if(rpin.test(pin) || pin.length != 6){
-        document.getElementById("pinlb").style.visibility = "visible";
-        document.getElementById("fpin").style.border = "2px solid red";
-        return false;
+    if(rpin.test(pin)){
+        document.getElementById("pinlb").innerHTML="*Only NUMBERS are Allowed";
+        document.getElementById("fp").style.border = "2px solid red";
+        flag = false;
+    }
+    else if(pin.length != 6){
+        document.getElementById("pinlb").innerHTML="*PIN Length > 6";
+        document.getElementById("fp").style.border = "2px solid red";
+        flag = false;
+    }
+    else{
+        document.getElementById("pinlb").innerHTML="";
+        document.getElementById("fp").style.border = "none";
     }
 
-    if(!rfe.test(fe)){
-        document.getElementById("elb").style.visibility = "visible";
+    if(!remail.test(email)){
+        document.getElementById("elb").innerHTML="*Ivalid EMAIL";
         document.getElementById("fe").style.border = "2px solid red";
-        return false;
-     }
+        flag = false;
+    }
+    else{
+        document.getElementById("elb").innerHTML="";
+        document.getElementById("fe").style.border = "none";
+    }
 
+    if(ckb1 == 0 && ckb2 == 0){
+        document.getElementById("ckblb").innerHTML="*Required Field";
+    }
+    else{
+        document.getElementById("ckblb").innerHTML="";
+    }
+
+    return flag;
 }
